c1debaec4067ec7843f33d1fe12beddddb76f249
strawberryskeleton <xradhi5685@gmail.com>
2026-07-17 21:43:59 +0530

    dog page: created (code almost identical to cat page) ++ tried more bg theme for cat page

---
+++ dog/dog.js
@@ -0,0 +1,36 @@
+
+const getNewBtn = document.getElementById('get-new-btn')
+const imgDiv = document.getElementById('img-div')
+
+getNewBtn.addEventListener('click', () => {
+    // fetchFromAPI()
+    displayAPIResult()
+})
+
+async function fetchFromAPI () {
+    try {
+        const response = await fetch('https://api.thedogapi.com/v1/images/search')
+
+        if(!response.ok) {
+            throw new Error(`API Fetching Error. ${response.status}`)
+        }
+
+        const data = await response.json()
+        console.log(data[0])
+        return data[0].url
+
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+async function displayAPIResult () {
+    imgURL = await fetchFromAPI()
+
+    // console.log(imgURL)
+
+    // console.log(imgDiv.innerHTML)
+
+    imgDiv.innerHTML = `<img src="${imgURL}" alt="dog img">`
+    console.log(imgDiv.innerHTML)
+}
